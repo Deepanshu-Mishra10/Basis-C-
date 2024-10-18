0e1f58fe527e78c296e2fcf8a8721a1ff3cbbddb
Deepanshu-Mishra10 <deepanshumishra1110@gmail.com>
2024-10-19 00:45:47 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    int o=9;
+    int p=45;
+    int c=o*p;
+    printf("The value of o is %d \n" ,o);
+    printf("The value of o is %d \n" ,p);
+    printf("The value of o is %d \n" ,c);
+}
---
+++ multiply2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    int o=9;
+    int p=7;
+    printf("The sum of o and p is %d" ,o*p);
+}
