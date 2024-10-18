b5a8e17f0accff462c38f7266dbc855c76da282c
Deepanshu-Mishra10 <deepanshumishra1110@gmail.com>
2024-10-19 00:12:11 +0530

    Add files via upload

---
+++ printthevalue.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    // int for integer %d
+    // char for character %c
+    // float for decimal number %f
+    // double for decimal number upto 5 digit %lf
+    int x=4;
+    printf("The vale of x is %d",x);
+}
