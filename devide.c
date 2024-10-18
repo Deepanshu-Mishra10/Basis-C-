11a859a1a259a66f701de8595b8d4b1286780bfa
Deepanshu-Mishra10 <deepanshumishra1110@gmail.com>
2024-10-19 00:48:47 +0530

    Add files via upload

---
+++ devide.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    float o=90;
+    float p=45;
+    float c=o/p;
+    printf("The value of o is %f \n" ,o);
+    printf("The value of o is %f \n" ,p);
+    printf("The value of o is %f \n" ,c);
+}
---
+++ devide2.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    float o=90;
+    float p=9;
+    printf("The sum of o and p is %f" ,o/p);
+}
