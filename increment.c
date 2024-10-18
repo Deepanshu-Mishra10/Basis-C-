1258b670ac1129ac498f3dd1cc82e3ffde8b2b5b
Deepanshu-Mishra10 <deepanshumishra1110@gmail.com>
2024-10-19 00:55:18 +0530

    Add files via upload

---
+++ increment.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    int o=9;
+    int i=o;
+    int p=45;
+    printf("The value of o is %d \n" ,++o);
+    printf("The value of o is %d \n" ,++p);
+    printf("The value of o is %d \n" ,i);
+}
