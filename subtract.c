900737811a5fa209cad0825ab0235381ac1d1bac
Deepanshu-Mishra10 <deepanshumishra1110@gmail.com>
2024-10-19 00:38:50 +0530

    Add files via upload

---
+++ subtract.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+    int o=90;
+    int p=45;
+    int c=o-p;
+    printf("The value of o is %d \n" ,o);
+    printf("The value of o is %d \n" ,p);
+    printf("The value of o is %d \n" ,c);
+}
